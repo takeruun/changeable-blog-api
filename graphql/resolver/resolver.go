612fd7c101093bf417e5375d04a43f6f606cb99e
takeru.hashimoto <hstake25@gmail.com>
2022-08-28 12:46:27 +0900

    resolver 修正

--- graphql/resolver/resolver.go
+++ graphql/resolver/resolver.go
@@ -1,11 +1,28 @@
 package resolver
 
-import "gorm.io/gorm"
+import (
+	"app/database"
+	"app/service"
+	"app/usecase"
+
+	"github.com/wader/gormstore/v2"
+)
 
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{
-	DB *gorm.DB
+type Resolver struct {
+	uc usecase.UsersUsecase
+}
+
+func NewResolver(db database.DB, store *gormstore.Store) *Resolver {
+	sss := service.NewSessionStoreService(store)
+
+	return &Resolver{
+		uc: usecase.UsersUsecase{
+			UsersRepo: &database.UsersRepository{DB: db},
+			SSService: sss,
+		},
+	}
 }
