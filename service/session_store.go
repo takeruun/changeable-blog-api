3e197d168a7e011dd33c8a4fcd57dc51b76b5d3d
takeru.hashimoto <hstake25@gmail.com>
2022-08-28 12:45:26 +0900

    session service 修正

--- service/session_store.go
+++ service/session_store.go
@@ -8,6 +8,21 @@ import (
 	"github.com/wader/gormstore/v2"
 )
 
+type SessionStoreService interface {
+	GetSession(ctx context.Context, name string) (*sessions.Session, error)
+	SaveSession(ctx context.Context, session *sessions.Session) error
+}
+
+type sessionStoreService struct {
+	store *gormstore.Store
+}
+
+func NewSessionStoreService(s *gormstore.Store) SessionStoreService {
+	return &sessionStoreService{
+		store: s,
+	}
+}
+
 // HTTPKey is the key used to extract the Http struct.
 type HTTPKey string
 
@@ -18,11 +33,11 @@ type HTTP struct {
 }
 
 // GetSession returns a cached session of the given name
-func GetSession(ctx context.Context, store *gormstore.Store, name string) (*sessions.Session, error) {
+func (service *sessionStoreService) GetSession(ctx context.Context, name string) (*sessions.Session, error) {
 	httpContext := ctx.Value(HTTPKey("http")).(HTTP)
 
 	// Ignore err because a session is always returned even if one doesn't exist
-	session, err := store.Get(httpContext.R, name)
+	session, err := service.store.Get(httpContext.R, name)
 	if err != nil {
 		return nil, err
 	}
@@ -31,10 +46,10 @@ func GetSession(ctx context.Context, store *gormstore.Store, name string) (*sess
 }
 
 // SaveSession saves the session by writing it to the response
-func SaveSession(ctx context.Context, store *gormstore.Store, session *sessions.Session) error {
+func (service *sessionStoreService) SaveSession(ctx context.Context, session *sessions.Session) error {
 	httpContext := ctx.Value(HTTPKey("http")).(HTTP)
 
-	err := store.Save(httpContext.R, *httpContext.W, session)
+	err := service.store.Save(httpContext.R, *httpContext.W, session)
 
 	return err
 }
